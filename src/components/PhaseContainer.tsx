700d4b383a2ecaadce6c94770fd8a92775e2f541
Rahul <rahuliodev8@gmail.com>
2020-05-10 23:37:47 -0400

    Test

--- src/components/PhaseContainer.tsx
+++ src/components/PhaseContainer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Box } from "@nulogy/components";
 
 export const PhaseContainer = ({ children }: any) => (
-  <Box border="1px solid blue" minHeight="500px" p="x1" m="x1">
+  <Box border="1px solid blue" minHeight="600px" p="x1" m="x1">
     {children}
   </Box>
 );
