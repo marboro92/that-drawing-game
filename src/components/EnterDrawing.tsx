afe8a9bf9bd6ca686e355a4d1fece347b36b8704
Marianne Borowiak <marianneb@nulogy.com>
2020-05-30 00:00:56 -0400

    add loading of neighbour's image

--- src/components/EnterDrawing.tsx
+++ src/components/EnterDrawing.tsx
@@ -2,36 +2,23 @@ import React, { useState, useContext } from "react";
 import { Text } from "@nulogy/components";
 import CanvasDraw from "react-canvas-draw";
 import { Button } from "./Button";
-import HostContext from "HostContext";
 
 type Props = {
-  children?: React.ReactNode;
+  phrase: string;
   onSubmit: Function;
 };
 
-export const EnterDrawing: React.FC<Props> = ({ onSubmit, children }) => {
+export const EnterDrawing: React.FC<Props> = ({ onSubmit, phrase }) => {
   const [canvasRef, setCanvasRef] = useState<any>(null);
-  const { room, playerName, roundNumber } = useContext(HostContext);
 
   const handleSubmit = () => {
     const drawing = canvasRef.getSaveData();
     onSubmit(drawing);
   };
 
-  const getPhrase = () => {
-    const playersInRoom: string[] = Object.keys(room.players).sort();
-    const indexOfCurrentPlayer = playersInRoom.indexOf(playerName);
-    const isLastPlayer = indexOfCurrentPlayer === playersInRoom.length - 1;
-    const neighboursName = isLastPlayer
-      ? playersInRoom[0]
-      : playersInRoom[indexOfCurrentPlayer + 1];
-    const phrase = room.players[neighboursName][roundNumber - 2] || "!!! BUG: MISSING PHRASE!!!";
-    return phrase;
-  };
-
   return (
     <>
-      <Text>{getPhrase()}</Text>
+      <Text>{phrase}</Text>
       <CanvasDraw ref={setCanvasRef} />
       <Button onClick={handleSubmit}>Submit</Button>
     </>
--- src/components/PlayingPhase.tsx
+++ src/components/PlayingPhase.tsx
@@ -15,8 +15,7 @@ type Props = {
 }
 
 const PlayingPhase: React.FC<Props> = ({ onSubmitContent }) => {
-  const { playerName, roomId, roundType, setRoundType } = useContext(HostContext);
-
+  const { playerName, roomId, roundType, setRoundType, roundNumber, room } = useContext(HostContext);
   const handleContentSubmit = async ({
     contentType,
     content,
@@ -34,17 +33,40 @@ const PlayingPhase: React.FC<Props> = ({ onSubmitContent }) => {
       playerName,
     });
   };
+
+  const getNeighboursContent = () => {
+    const playersInRoom: string[] = Object.keys(room.players).sort();
+    const indexOfCurrentPlayer = playersInRoom.indexOf(playerName);
+    const isLastPlayer = indexOfCurrentPlayer === playersInRoom.length - 1;
+    const neighboursName = isLastPlayer
+      ? playersInRoom[0]
+      : playersInRoom[indexOfCurrentPlayer + 1];
+    const neighboursContent = room.players[neighboursName]
+    console.log("neig", neighboursContent)
+    return neighboursContent
+  };
+
+  const getContent = () => {
+    const phrase = getNeighboursContent()[roundNumber - 2];
+    return phrase;
+  };
+
+  const phrase = roundType === CONTENT_TYPES.drawing && roundNumber > 1 ? getContent() : null;
+  const image = roundType === CONTENT_TYPES.phrase && roundNumber > 1 ? getContent() : null;
+
   return roundType === CONTENT_TYPES.phrase ? (
     <EnterPhrase
       onSubmit={(content: string) =>
         handleContentSubmit({ contentType: CONTENT_TYPES.phrase, content })
       }
+      image={image}
     />
   ) : (
       <EnterDrawing
         onSubmit={(content: string) =>
           handleContentSubmit({ contentType: CONTENT_TYPES.drawing, content })
         }
+        phrase={phrase}
       />
     )
 }
